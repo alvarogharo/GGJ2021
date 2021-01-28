309ef8ed2be6bf3c22707b18395c76a95b29d3c4
Álvaro <alvarogharo@gmail.com>
2021-01-28 21:51:16 +0100

    Added outline controller + fase controller

---
+++ Assets/Scripts/IntratableFasesController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntratableFasesController : MonoBehaviour
+{
+    public string[] interactableObjets;
+    public int currentFase = 0;
+    private List<OutlineController> currentFaseControllers;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        currentFaseControllers = GetOutlineControllersByFase(currentFase);
+        EnableFase(currentFase);
+    }
+    public void EnableFase(int fase)
+    {
+        SwitchInteractibleFromControllers(currentFaseControllers, false);
+        currentFaseControllers = GetOutlineControllersByFase(fase);
+        SwitchInteractibleFromControllers(currentFaseControllers, true);
+    }
+
+    private List<OutlineController> GetOutlineControllersByFase(int fase)
+    {
+        string faseNames = interactableObjets[fase];
+        string[] faseNamesSplit = faseNames.Split(',');
+        List<OutlineController> outControllers = new List<OutlineController>();
+        foreach(string name in faseNamesSplit)
+        {
+            outControllers.Add(GameObject.Find(name).GetComponent<OutlineController>());
+        }
+        return outControllers;
+    }
+
+    private void SwitchInteractibleFromControllers(List<OutlineController> outControllers, bool newState)
+    {
+        foreach (OutlineController controller in outControllers)
+        {
+            controller.isInteractable = newState;
+        }
+    }
+}
