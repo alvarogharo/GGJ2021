e321f87f12261758d6a0d05f791a0d14200c282f
Álvaro <alvarogharo@gmail.com>
2021-01-29 19:36:38 +0100

    Added camera shake controller

---
+++ Assets/Scripts/Camera/CameraShakeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EZCameraShake;
+
+public class CameraShakeController : MonoBehaviour
+{
+    public float defaultMagnitude;
+    public float defaultRoughness;
+    public float defaultFadeInTime;
+    public float defaultFadeOutTime;
+
+    public void ShakeDefault()
+    {
+        CameraShaker.Instance.ShakeOnce(defaultMagnitude, defaultRoughness, defaultFadeInTime, defaultFadeOutTime);
+    }
+
+    public void Shake(float magnitude, float roughness, float fadeInTime, float fadeOutTime)
+    {
+        CameraShaker.Instance.ShakeOnce(magnitude, roughness, fadeInTime, fadeOutTime);
+    }
+}
