9b5ff2bad248b8a975f6da48bbcfc38175729144
Ivan Uriel Jimenez <iviuriel.pictures@gmail.com>
2021-01-30 12:27:34 +0100

    Complete dialogue + control phase first review

--- Assets/Scripts/IntratableFasesController.cs
+++ Assets/Scripts/IntratableFasesController.cs
@@ -6,18 +6,38 @@ public class IntratableFasesController : MonoBehaviour
 {
     public string[] interactableObjets;
     public int currentFase = 0;
+    public int[] numberOfKeyObjectPerPhase;
     private List<InteractableObjectController> currentFaseControllers;
+    private bool[] keyObjectFaseController;
     // Start is called before the first frame update
     private void Awake()
     {
+        keyObjectFaseController = new bool[numberOfKeyObjectPerPhase[currentFase]];
+        for(int i = 0; i<keyObjectFaseController.Length; i++){
+            keyObjectFaseController[i] = false;
+        }
         currentFaseControllers = GetOutlineControllersByFase(currentFase);
         EnableFase(currentFase);
+    }
+
+    private void Update() {
+        for(int i = 0; i<keyObjectFaseController.Length; i++){
+            if(!keyObjectFaseController[i]){
+                return;
+            }
+        }
+
+        EnableFase(currentFase+1);
     }
     public void EnableFase(int fase)
     {
         SwitchInteractibleFromControllers(currentFaseControllers, false);
         currentFaseControllers = GetOutlineControllersByFase(fase);
         SwitchInteractibleFromControllers(currentFaseControllers, true);
+        keyObjectFaseController = new bool[numberOfKeyObjectPerPhase[currentFase]];
+        for(int i = 0; i<keyObjectFaseController.Length; i++){
+            keyObjectFaseController[i] = false;
+        }
         currentFase = fase;
     }
 
@@ -39,5 +59,9 @@ public class IntratableFasesController : MonoBehaviour
         {
             controller.isInteractable = newState;
         }
+    }
+
+    public void UseKeyObject(int index){
+        keyObjectFaseController[index] = true;
     }
 }
