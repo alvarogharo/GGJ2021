ba198ffd8cc2b55ac546622bff99fa2982815907
Álvaro <alvarogharo@gmail.com>
2021-01-30 13:11:33 +0100

    Added some intect audios and volumens + cinematics controller

---
+++ Assets/Scripts/Animations/CinematicsController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CinematicsController : MonoBehaviour
+{
+    public int currentAnimation;
+    private Animator[] cinematicsAnimator;
+
+    void Start()
+    {
+        cinematicsAnimator = gameObject.GetComponentsInChildren<Animator>();
+        currentAnimation = 0;
+    }
+
+    public void PlayCurrentAnimationAndUpdateState()
+    {
+        if (currentAnimation < cinematicsAnimator.Length)
+        {
+            cinematicsAnimator[currentAnimation].SetTrigger("Play");
+            currentAnimation++;
+        }
+    }
+}
