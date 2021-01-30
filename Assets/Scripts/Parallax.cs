bfc1587624363526448edc48d3de20f8f99032f8
Alex <alexpalvarez@protonmail.com>
2021-01-30 02:12:43 +0100

    Parallax script for orthographic cameras, needs finetuning

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[ExecuteInEditMode]
+public class Parallax : MonoBehaviour{
+
+    public Camera camera;
+
+    private float smooth = 0f;
+
+    public Vector3 realPosition;
+    public Vector3 correctedPosition;
+
+
+    public float A;
+    public float B;
+    public float C;
+    public float D;
+    
+    void Start(){
+        realPosition = transform.position;
+        A = 0; B = 0; C = 0; D = 0;
+    }
+
+    void Update(){
+        float correctedX = calculateCorrectedX();
+        correctedPosition = new Vector3(correctedX, transform.position.y, transform.position.z);
+        transform.position = correctedPosition;
+    }
+
+    public float calculateCorrectedX(){
+
+        /*
+
+                                [object]
+                                  /|
+                                /  |
+                             /     |
+                          / |      |
+                       /    | (A)  | B
+         [camera]() /_ _ _ _| _ _ _|        [z axis]
+                        C
+                            D
+
+        */
+
+        int sign = 1;
+
+        C = Mathf.Abs(camera.transform.position.z);
+        D = C + Mathf.Abs(transform.position.z);
+        B = realPosition.x - camera.transform.position.x;
+        sign = B > 0 ? 1 : -1; 
+        B = Mathf.Abs(B);
+
+        A = (B*C)/D;
+
+        A = A + ( (A - B) * smooth );
+
+        A = A * sign;
+
+        return A;
+    }
+
+
+}
