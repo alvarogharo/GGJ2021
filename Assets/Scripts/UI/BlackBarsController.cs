7b6b854699353b0a44ee5977171757556eab88e6
Álvaro <alvarogharo@gmail.com>
2021-01-30 11:08:13 +0100

    Added black bars animations

---
+++ Assets/Scripts/UI/BlackBarsController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackBarsController : MonoBehaviour
+{
+    private Animator blackBarsAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        blackBarsAnimator = GetComponent<Animator>();
+    }
+
+    public void ShowBlackBars()
+    {
+        SetBlackBarsState(true);
+    }
+
+    public void HideBlackBars()
+    {
+        SetBlackBarsState(false);
+    }
+
+    private void SetBlackBarsState(bool areBarsIn)
+    {
+        blackBarsAnimator.SetBool("areBarsIn", areBarsIn);
+    }
+}
