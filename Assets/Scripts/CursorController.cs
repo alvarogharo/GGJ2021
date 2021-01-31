6035636c377bde6b4c2bf2609ce6b848d5bdce43
Álvaro <alvarogharo@gmail.com>
2021-01-31 09:43:54 +0100

    Added hand coursor control

--- Assets/Scripts/CursorController.cs
+++ Assets/Scripts/CursorController.cs
@@ -5,120 +5,60 @@ using UnityEngine;
 
 public class CursorController : MonoBehaviour
 {
-    public Texture2D cursor;
-    public Texture2D cursorClicked;
-    public Texture2D cursorUp;
-    public Texture2D cursorDown;
-    public Texture2D cursorLeft;
-    public Texture2D cursorRight;
-    public float horizontalOffset;
-    public float verticalOffset;
-    public int stateChangeDelay;
+    public float cursorChangeOffSet;
 
-    public CursorState currentState;
-
-    private Texture2D[] cursorTypes;
-    public Vector3 mousePos = Vector3.zero;
-    public Vector3 prevMousePos = Vector3.zero;
-    private bool firstFrame = true;
-    private int stateCount = 0;
+    private Animator cursorAnimator;
+    private SpriteRenderer sprRenderer;
     private AudioSource audioSrc;
-
-    public enum CursorState
-    {
-        None = 0,
-        Clicked = 1,
-        Up = 2,
-        Down = 3,
-        Left = 4,
-        Right = 5
-    }
+    private Vector3 mousePos;
+    private Vector3 prevMousePos;
 
     // Start is called before the first frame update
     void Awake()
     {
-        Texture2D[] cursorTypesInit = { cursor, cursorClicked, cursorUp, cursorDown, cursorLeft, cursorRight };
-        cursorTypes = cursorTypesInit;
-        currentState = CursorState.None;
-        ChangeCursor(cursorTypes[(int)currentState]);
-        //HideCursor();
-        DontDestroyOnLoad(this.gameObject);
+        cursorAnimator = gameObject.GetComponent<Animator>();
+        sprRenderer = gameObject.GetComponent<SpriteRenderer>();
         audioSrc = gameObject.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Cursor.visible = false;
         mousePos = Input.mousePosition;
         Vector3 mouseMotion = prevMousePos - mousePos;
-        if (stateCount < stateChangeDelay && (currentState == CursorState.Up || currentState == CursorState.Down || currentState == CursorState.Left || currentState == CursorState.Right))
+
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(worldPosition.x, worldPosition.y, -7);
+
+        if (mouseMotion.x > cursorChangeOffSet)
         {
-            stateCount++;
+            cursorAnimator.SetBool("isGoingLeft", true);
+        } else if (mouseMotion.y < -cursorChangeOffSet)
+        {
+            cursorAnimator.SetBool("isGoingLeft", false);
         }
-        else
+
+        if (Input.GetMouseButtonDown(0))
         {
-            stateCount = 0;
-            if (!Input.GetMouseButton(0))
+            cursorAnimator.SetTrigger("click");
+            if (!audioSrc.isPlaying)
             {
-                if ((mouseMotion.x <= horizontalOffset && mouseMotion.x >= -horizontalOffset) || (mouseMotion.y <= verticalOffset && mouseMotion.y >= -verticalOffset))
-                {
-                    ChangeCursor(cursor);
-                }
-
-                if (Math.Abs(mouseMotion.x) >= Math.Abs(mouseMotion.y))
-                {
-                    if (mouseMotion.x > horizontalOffset)
-                    {
-                        ChangeCursor(cursorLeft);
-                    }
-                    else if (mouseMotion.x < -horizontalOffset)
-                    {
-                        ChangeCursor(cursorRight);
-                    }
-                }
-                else
-                {
-
-                    if (mouseMotion.y > verticalOffset)
-                    {
-                        ChangeCursor(cursorDown);
-                    }
-                    else if (mouseMotion.y < -verticalOffset)
-                    {
-                        ChangeCursor(cursorUp);
-                    }
-                }
+                audioSrc.Play(0);
             }
-        }
-        if (Input.GetMouseButtonDown(0) && !audioSrc.isPlaying)
-        {
-            audioSrc.Play(0);
-        }
-        if (Input.GetMouseButton(0))
-        {
-            ChangeCursor(cursorClicked);
-        }
-        else if (Input.GetMouseButtonUp(0))
-        {
-            ChangeCursor(cursor);
-        }
-
-        if (firstFrame)
-        {
-            ChangeCursor(cursor);
-            firstFrame = false;
         }
 
         prevMousePos = mousePos;
     }
 
-    private void ChangeCursor(Texture2D cursorType)
+    public void ShowCursor()
     {
-        currentState = (CursorState)Array.IndexOf(cursorTypes, cursorType);
-        Cursor.SetCursor(cursorType, Vector2.zero, CursorMode.Auto);
+        sprRenderer.enabled = true;
     }
 
-    public void ShowCursor(){Cursor.visible = true;}
-    public void HideCursor(){Cursor.visible = false;}
+    public void HideCursor()
+    {
+        sprRenderer.enabled = false;
+    }
 
 }
