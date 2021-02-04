1e6bcb75e113ec2570eda14800bb33150f946c39
jordoe <jordoe@hotmail.com>
2021-02-04 17:14:42 +0100

    hand stopps when click and return smoothly to cursor when animation ends

--- Assets/Scripts/CursorController.cs
+++ Assets/Scripts/CursorController.cs
@@ -5,39 +5,40 @@ using UnityEngine;
 
 public class CursorController : MonoBehaviour
 {
-    public float cursorChangeOffSet;
-    public float moveSpeed;
+    public float defaultMoveSpeed;
 
     private Animator cursorAnimator;
     private SpriteRenderer sprRenderer;
     private AudioSource audioSrc;
     private Vector3 mousePos;
-    private Vector3 prevMousePos;
+    private float moveSpeed;
+    private bool blockedClick;
 
     void Awake()
     {
         cursorAnimator = gameObject.GetComponent<Animator>();
         sprRenderer = gameObject.GetComponent<SpriteRenderer>();
         audioSrc = gameObject.GetComponent<AudioSource>();
-        Cursor.visible = false;
+        moveSpeed = defaultMoveSpeed;
+        blockedClick = false;
     }
 
     void Update()
     {
         Cursor.visible = false;
 
-        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        // transform.position = new Vector3(mouseWorldPosition.x, mouseWorldPosition.y, -7);
-
-        transform.position = Vector2.Lerp(transform.position, mouseWorldPosition, moveSpeed * Time.deltaTime);
-
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && !blockedClick)
         {
             cursorAnimator.SetTrigger("click");
+            moveSpeed = 0.1f;
+            blockedClick = true;
             if (!audioSrc.isPlaying)
             {
                 audioSrc.Play(0);
             }
+        } else {
+            Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = Vector2.Lerp(transform.position, mouseWorldPosition, moveSpeed * Time.deltaTime);
         }
     }
 
@@ -45,6 +46,20 @@ public class CursorController : MonoBehaviour
     {
         setAnimationState();
     }
+
+    public void startClickToMoveTransition() {
+        StartCoroutine(clickToMoveTransition (moveSpeed, defaultMoveSpeed, 1));
+    }
+
+    private IEnumerator clickToMoveTransition (float oldValue, float newValue, float duration) {
+        for (float t = 0f; t < duration; t += Time.deltaTime) {
+            moveSpeed = Mathf.Lerp(oldValue, newValue, t / duration);
+            yield return null;
+        }
+        moveSpeed = newValue;
+        blockedClick = false;
+    }
+
 
     private void setAnimationState() {
         if(Input.GetAxis("Mouse X") < 0) {
@@ -64,9 +79,4 @@ public class CursorController : MonoBehaviour
     {
         sprRenderer.enabled = false;
     }
-
-    public void ClickAnimationEnded() {
-        Debug.Log("Terminó");
-    }
-
 }
