00c0d8270c37647f34a214a9a1300bbb9ccb62a9
Alex <alexpalvarez@protonmail.com>
2021-02-09 22:43:33 +0100

    New parallax, it's semi working + kitchen wall and lights

--- Assets/Scripts/Camera/Parallax.cs
+++ Assets/Scripts/Camera/Parallax.cs
@@ -7,27 +7,51 @@ public class Parallax : MonoBehaviour{
 
     public Camera camera;
 
+    private float previousCameraPos;
+    private float cameraDistanceMoved;
+
     private float smooth = 0f;
 
 
+    #region vars Parallax con el teorema de Thales
+
     public Vector3 realPosition;
     private Vector3 correctedPosition;
-
 
     private float A;
     private float B;
     private float C;
     private float D;
+    #endregion
     
     void Start(){
-        //realPosition = transform.position;
-        //A = 0; B = 0; C = 0; D = 0;
+        realPosition = transform.position;
+        A = 0; B = 0; C = 0; D = 0;
+
+        previousCameraPos = camera.transform.position.x;
     }
 
     void Update(){
-        //float correctedX = calculateCorrectedX();
-        //correctedPosition = new Vector3(correctedX, transform.position.y, transform.position.z);
-        //transform.position = correctedPosition;
+        //parallaxThales();
+        parallaxV2();
+    }
+
+    #region Parallax con desplazamiento de cámara
+    
+    public void parallaxV2(){
+        cameraDistanceMoved = previousCameraPos - camera.transform.position.x;
+        transform.Translate( (cameraDistanceMoved * transform.position.z) / (camera.transform.position.z), 0, 0);
+        previousCameraPos = camera.transform.position.x;
+    }
+
+    #endregion
+
+    #region Parallax con el teorema de Thales
+
+    public void parallaxThales(){        
+        float correctedX = calculateCorrectedX();
+        correctedPosition = new Vector3(correctedX, transform.position.y, transform.position.z);
+        transform.position = correctedPosition;
     }
 
     public float calculateCorrectedX(){
@@ -62,6 +86,6 @@ public class Parallax : MonoBehaviour{
 
         return corX;
     }
-
+    #endregion
 
 }
